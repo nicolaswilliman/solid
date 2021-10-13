ca3b1343cf05ef34caa04fe913a6500c5b39afba
willy <nicolas.williman@stensul.com>
2021-10-13 19:43:26 -0300

    removed unnecessary methods

--- DependencyInversionPrinciple/Correction.php
+++ DependencyInversionPrinciple/Correction.php
@@ -3,7 +3,6 @@
 interface UserRepositoryInterface
 {
     public function getFromDate($date);
-    public function create(array $data);
 }
 
 class UserRepository implements UserRepositoryInterface
@@ -11,16 +10,6 @@ class UserRepository implements UserRepositoryInterface
     public function getFromDate($date)
     {
         return User::where('created_at', '>=', $date)->get();
-    }
-
-    public function create(array $data)
-    {
-        $user = new User;
-        $user->name = $data['name'];
-        $user->email = $data['email'];
-        $user->save();
-
-        return $user;
     }
 }
 
